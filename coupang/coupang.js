94d2e589c83842a6e571997b4a38a94a24422a5b
Wan Sim <0420syj@naver.com>
2022-04-27 18:12:59 +0000

    리팩토링

--- coupang/coupang.js
+++ coupang/coupang.js
@@ -12,35 +12,37 @@ const toStringByFormatting = (src, delimiter = '-') => {
     return [year, month, date].join(delimiter)
 }
 
-// 최종결과 초기화
-let result = ""
+// 현재 페이지 전체 스캔 및 출력
+const scanCurrentPage = () => {
+    // 최종결과 초기화
+    let result = ""
 
-// 날짜별 주문 스캔
-const itemList = document.getElementsByClassName('sc-fimazj-0 dXMjMM')
+    // 날짜별 주문 스캔
+    const itemList = document.getElementsByClassName('sc-fimazj-0 dXMjMM')
 
-// 주문 스캔 시작
-for (let item of itemList) {
-    // 출력값 초기화
-    let output = ""
+    // 주문 스캔 시작
+    for (let item of itemList) {
+        // 출력값 초기화
+        let output = ""
 
-    // 주문 날짜
-    const date = item.getElementsByClassName('sc-abukv2-1 cRCSWJ')[0].innerText.slice(0, -3)
-    const dateArray = date.split('.')
-    const printDate = toStringByFormatting(new Date(dateArray[0], dateArray[1] - 1, dateArray[2]))
+        // 주문 날짜
+        const date = item.getElementsByClassName('sc-abukv2-1 cRCSWJ')[0].innerText.slice(0, -3)
+        const dateArray = date.split('.')
+        const printDate = toStringByFormatting(new Date(dateArray[0], dateArray[1] - 1, dateArray[2]))
 
-    const productName = item.getElementsByClassName('sc-755zt3-1 sc-8q24ha-1 fZLyLY gcWFxB')
-    const priceAndAmount = item.getElementsByClassName('sc-uaa4l4-0 KaKJy')
-    for (let i = 0; i < productName.length; i++) {
-        const price = priceAndAmount[i].getElementsByTagName('span')[0].innerText.slice(0, -2).replace(',', '')
-        const amount = priceAndAmount[i].getElementsByTagName('span')[3].innerText.slice(0, -2)
+        const productName = item.getElementsByClassName('sc-755zt3-1 sc-8q24ha-1 fZLyLY gcWFxB')
+        const priceAndAmount = item.getElementsByClassName('sc-uaa4l4-0 KaKJy')
+        for (let i = 0; i < productName.length; i++) {
+            const price = priceAndAmount[i].getElementsByTagName('span')[0].innerText.slice(0, -2).replace(',', '')
+            const amount = priceAndAmount[i].getElementsByTagName('span')[3].innerText.slice(0, -2)
 
-        output += printDate + '\t'
-        output += productName[i].innerText + '\t'
-        output += (price * amount) + '\t🏠 생활' + '\t💳 신용카드' + '\t쿠팡\n'
+            output += printDate + '\t'
+            output += productName[i].innerText + '\t'
+            output += (price * amount) + '\t🏠 생활' + '\t💳 신용카드' + '\t쿠팡\n'
+        }
+        result += output
     }
-
-    // console.log(output)
-    result += output
+    return result
 }
 
-console.log(result)
+console.log(scanCurrentPage())
